5d3af9d2e409188863005d03aba2327877f9c838
Zhenyu Wang <zhenyuw@linux.intel.com>
2015-07-07 12:17:42 +0800

    Change to melpa.org site and upgrade packages

--- elpa/ggtags-0.6.7/ggtags-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "ggtags" "0.6.7" "GNU Global source code tagging system" (quote nil))
--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages"))
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/"))
 
 (package-initialize)
 
