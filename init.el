41ede05738889d3c3219d5a44da4123075d57827
Zhenyu Wang <zhenyuw@linux.intel.com>
2015-07-07 13:20:48 +0800

    Always require for package check

--- init.el
+++ init.el
@@ -66,6 +66,7 @@
 (require 'tablegen-mode)
 
 ;; paredit (from emacswiki)
+(require 'paredit)
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
