c46675d405f2576023126031388456349fa6d2b1
Zhenyu Wang <zhenyuw@linux.intel.com>
2019-06-13 02:59:44 +0000

    remove unnecessary tweaks now

--- init.el
+++ init.el
@@ -3,9 +3,7 @@
 
 ;; be quiet, bady
 (blink-cursor-mode 0)
-(scroll-bar-mode 0)
 (menu-bar-mode 0)
-(tool-bar-mode 0)
 (setq inhibit-splash-screen t)
 (setq initial-scratch-message "")
 ;;(setq visible-bell t)
