ca977bfe423df67c5d10b3c41147ebac8fc5809c
Zhenyu Wang <zhenyuw@linux.intel.com>
2020-06-16 14:16:50 +0800

    init.el update

--- init.el
+++ init.el
@@ -4,18 +4,23 @@
 ;; be quiet, bady
 (blink-cursor-mode 0)
 (menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
 (setq inhibit-splash-screen t)
 (setq initial-scratch-message "")
 ;;(setq visible-bell t)
 (setq default-fill-column 80)
 
+;; font and size
+(set-frame-font "Hack-14" nil t)
+
 ;; package (ELPA/MELPA)
+;;	("gnu" . "http://elpa.gnu.org/packages")
+;;	("marmalade" . "http://marmalade-repo.org/packages")
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages")
-	("marmalade" . "http://marmalade-repo.org/packages")
-	("melpa" . "http://melpa.org/packages/")))
+      '(("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 ;; 'use-package'
@@ -63,6 +68,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (vc-msg slime zenburn-theme racer rust-mode use-package paredit org lua-mode go-playground ggtags geiser)))
  '(send-mail-function (quote smtpmail-send-it))
  '(smtpmail-smtp-server "mail.intel.com")
  '(smtpmail-smtp-service 25))
@@ -98,3 +106,17 @@
   "Show the full path file name in the minibuffer."
   (interactive)
   (message (buffer-file-name)))
+
+;; rust & racer
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+
+;; slime
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
+
+;; recent files
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
