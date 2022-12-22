49b3384a4e5d891c8ae190afee011fefac7e5316
Zhenyu Wang <zhenyuw@linux.intel.com>
2022-12-22 17:05:35 +0800

    Add note doc for init.el

--- init.el
+++ init.el
@@ -1,1 +1,10 @@
+;; This file is supposed to be copied as default init.el,
+;; which would load more static config and hold any local setting.
+;; This can also be for local experimental before moving to config.el
+
+;; if requiring proxy setting for initial package install
+;; (setq url-proxy-services
+;;       '(("http" . "proxy:port")
+;;         ("https" . "proxy:port")))
+
 (load-file "~/.emacs.d/sync/config.el")
