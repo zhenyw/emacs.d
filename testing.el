6ce70b56ba1322ba5b6224d42ff26d1953b07d4c
Zhenyu Wang <zhenyuw@linux.intel.com>
2023-08-10 10:30:35 +0800

    Switch to eglot in emacs 29.1
    lsp-mode is not that bad, but this wants to try eglot more as it's included
    in emacs 29.1.

--- testing.el
+++ testing.el
@@ -59,3 +59,62 @@
 
 (use-package deadgrep)
 
+;;(use-package lsp-mode
+;;  :init
+;;  :bind-keymap ("C-c l" . lsp-command-map)
+;;  :hook (;;lsp-rust: need to install rust-analyzer like
+;;	 ;;    rustup component add rust-src
+;;	 ;;    rustup component add rust-analyzer
+;;	 ;;Need to add 'rust-analyzer' into PATH e.g
+;;	 ;;    ln -sf `rustup which rust-analyzer` ~/bin/.
+;;	 ;;lsp-rust would by default search 'rust-analyzer' binary
+;;	 ;;Note: maybe there's some cargo metadata error, looks if
+;;	 ;;tried to build project one, it might better to go...
+;;	 (rust-mode
+;;	  ;;enable for all c/c++ mode (not sure why cc-mode not work...)
+;;	  ;;for linux kernel, just use 'clangd' lsp server
+;;	  ;;   make CC=clang defconfig (for e.g)
+;;	  ;;   make CC=clang
+;;	  ;;   ./scripts/clang-tools/gen_compile_commands.py
+;;	  ;;then just import project from top of linux kernel source
+;;	  ;;default xref key seems still works:
+;;	  ;;   alt+.: find definition
+;;	  ;;   S-alt-?: find reference
+;;	  c-mode
+;;	  c++-mode
+;;	  c-or-c++-mode
+;;	  ) . lsp-deferred)
+;;  :commands lsp
+;;  :config
+;;  (lsp-enable-which-key-integration t))
+
+;;(use-package lsp-ui
+;;  :custom-face
+;;  ;; make peek window same color as default peek list window
+;;  ;; make color similar to zenburn theme default background
+;;  (lsp-ui-peek-peek ((t (:background "#383838"))))
+;;  (lsp-ui-peek-list ((t (:background "#383838"))))
+;;  :hook (lsp-mode . lsp-ui-mode)
+;;  :bind
+;;  (("M-]" . lsp-ui-peek-find-definitions)
+;;   )
+;;  :custom
+;;  ;; peek keybind
+;;  ;; n/p: next/prev
+;;  ;; M-n/p: 
+;;  ;; enter: open file
+;;  ;; M-enter: open file in new window
+;;  ;; q: quit
+;;  ((lsp-ui-peek-enable t)
+;;   (lsp-ui-peek-always-show t)
+;;   )
+;;  )
+
+
+;; eglot
+(use-package eglot
+  :config
+  (setq eglot-report-progress t)
+  (add-hook 'rust-mode-hook 'eglot-ensure)
+  (add-hook 'c-mode-hook 'eglot-ensure)
+  )
