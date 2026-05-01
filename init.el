92fa43350f041f61957e476530ce03e0081527c9
Zhenyu Wang <zhenyuw.linux@gmail.com>
2026-05-01 12:16:56 +0900

    custom-file:split custom from init.el

--- init.el
+++ init.el
@@ -7,6 +7,11 @@
 ;;       '(("http" . "proxy:port")
 ;;         ("https" . "proxy:port")))
 
+;; Without the `custom-file', Emacs writes directly to the "init.el",
+;; which can be confusing.
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
+
 (load-file "~/.emacs.d/sync/config.el")
 
 ;; load under testing config?
